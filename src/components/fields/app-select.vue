8652eb191e4fbd4966c9ee62027ecc1978edc259
IraDmit <alisagoodpirate@gmail.com>
2024-03-27 16:00:11 +0400

    style app select

--- src/components/fields/app-select.vue
+++ src/components/fields/app-select.vue
@@ -51,3 +51,86 @@ const getOptionName = (option: Locations | Authors): string => {
     return '';
 };
 </script>
+
+<style scoped lang="scss">
+.select {
+    border: 1px solid var(--placeholder-color);
+    border-radius: 8px;
+    background: var(--bg-filter);
+    cursor: pointer;
+    color: var(--txt-color);
+    font-family: Roboto;
+    position: relative;
+
+    .placeholder {
+        padding: 15px;
+        display: flex;
+        justify-content: space-between;
+
+        .txt {
+
+            font-family: Roboto;
+            font-size: 13px;
+            font-weight: 400;
+            line-height: 15px;
+            letter-spacing: 0px;
+            color: var(--txt-color);
+        }
+
+        .actions {
+            display: flex;
+            grid-gap: 10px;
+        }
+
+    }
+
+    &.active {
+        border: 1px solid var(--border);
+        border-radius: 8px 8px 0 0;
+    }
+
+    .options {
+        position: absolute;
+        top: 100%;
+        left: 50%;
+        transform: translateX(-50%);
+        width: calc(100% + 2px);
+        z-index: 10;
+        max-height: 345px;
+        overflow-y: scroll;
+        background-color: var(--bg);
+        border-radius: 0 0 8px 8px;
+        opacity: 0;
+        visibility: hidden;
+        border: 1px solid var(--border);
+        border-top: 1px solid var(--placeholder-color);
+
+        .item-option {
+            padding: 15px 20px;
+
+            &:hover {
+                background-color: var(--active-bg);
+                color: var(--active-txt)
+            }
+        }
+
+        &.open {
+            opacity: 1;
+            visibility: visible;
+        }
+    }
+
+    .options::-webkit-scrollbar {
+        background: var(--bg);
+        border-radius: 5px;
+        width: 5px;
+        margin-right: 8px;
+    }
+
+    .options::-webkit-scrollbar-thumb {
+        background: rgb(123, 123, 123);
+        border-radius: 10px;
+        box-shadow: 0px 2px 2px 0px rgba(0, 0, 0, 0.12);
+    }
+}
+</style>
